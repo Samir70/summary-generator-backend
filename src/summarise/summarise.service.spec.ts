1eb8f701387e4d828eabe11604b590d6f8525c56
Samir70 <sbetmouni@talktalk.net>
2023-04-12 16:19:35 +0100

    refactor tests -- better description

--- src/summarise/summarise.service.spec.ts
+++ src/summarise/summarise.service.spec.ts
@@ -15,26 +15,17 @@ describe('SummariseService', () => {
     it('should be defined', () => {
         expect(service).toBeDefined();
     });
-});
 
-describe('SummariseService -- without sending requests to openai', () => {
-    let service: SummariseService;
-
-    beforeEach(async () => {
-        const module: TestingModule = await Test.createTestingModule({
-            providers: [SummariseService],
-        }).compile();
-
-        service = module.get<SummariseService>(SummariseService);
+    describe('getSummary method -- without sending requests to openai', () => {
+        it("should respond with long summary of particular short poem", async () => {
+            const result = await service.getSummary("frog pond splash")
+            expect(result).toEqual({ text: "frog pond splash", summary: "The text you submitted can be summarised as follows: It is a translation of a haiku expressing the inevitability of actions when free will is nothing but an illusion." })
+        })
+        it("should respond with sarcasm to text with fewer than 20 words", async () => {
+            const result = await service.getSummary("A frog sees a pond and jumps in.")
+            expect(result).toEqual({ text: "A frog sees a pond and jumps in.", summary: "That's so much to summarise. Give me a moment, I'm still reading." })
+        })
     });
-
-    it("should respond with long summary of particular short poem", async () => {
-        const result = await service.getSummary("frog pond splash")
-        expect(result).toEqual({ text: "frog pond splash", summary: "The text you submitted can be summarised as follows: It is a translation of a haiku expressing the inevitability of actions when free will is nothing but an illusion." })
-    })
-    it("should respond with sarcasm to text with fewer than 20 words", async () => {
-        const result = await service.getSummary("A frog sees a pond and jumps in.")
-        expect(result).toEqual({ text: "A frog sees a pond and jumps in.", summary: "That's so much to summarise. Give me a moment, I'm still reading." })
-    })
 });
+
 
